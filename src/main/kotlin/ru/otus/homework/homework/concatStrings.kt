e26d25eeccb99f21ac0ffc77a3f2dc0f1dff0cca
user <ianinaostashchenko@gmail.com>
2025-10-19 12:45:51 +0300

    HW Task3 added test function concatStrings

--- src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
+++ src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
@@ -1,11 +1,22 @@
 package ru.otus.homework.homework
 
 fun main() {
-    check("str1,str2,str3" == concatStrings("str1", "str2", "str3", char = ','))
     check("str1 str2 str3" == concatStrings("str1", "str2", "str3"))
-    check("str1:str2:str3" == concatStrings("str1", "str2", "str3", char = ':'))
 }
 
 fun concatStrings(vararg strings: String, char: Char = ' '): String {
     return strings.joinToString(separator = char.toString())
 }
+
+fun testConcatStrings() {
+    val str1 = "str1"
+    val str2 = "str2"
+    val str3 = "str3"
+
+    val resultDefault = concatStrings(str1, str2, str3)
+    check(resultDefault == "str1 str2 str3") { "Expected 'str1 str2 str3', but got '$resultDefault'" }
+
+    val resultComma = concatStrings(str1, str2, str3, char = ',')
+    check(resultComma == "str1,str2,str3") { "Expected 'str1,str2,str3', but got '$resultComma'" }
+}
+
