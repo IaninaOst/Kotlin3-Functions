d38e9de267261d16ca56ba9f03179d8e8edf6391
user <ianinaostashchenko@gmail.com>
2025-10-19 12:48:40 +0300

    HW Task3 added test function call (testConcatStrings) to main

--- src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
+++ src/main/kotlin/ru/otus/homework/homework/concatStrings.kt
@@ -1,7 +1,7 @@
 package ru.otus.homework.homework
 
 fun main() {
-    check("str1 str2 str3" == concatStrings("str1", "str2", "str3"))
+    testConcatStrings()
 }
 
 fun concatStrings(vararg strings: String, char: Char = ' '): String {
